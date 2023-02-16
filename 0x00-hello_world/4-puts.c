d8d46f379ec6403a4a4b1310f3d3370642ed0472
reginaldbdanso <reginaldbdanso@gmail.com>
2023-02-16 07:21:57 -0800

    added script 4 and fixed main.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Succcess)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -2,10 +2,10 @@
 
 /**
  * main - Entry point
- * 
- *Return: Always 0 (Success)
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-  return (0);
+return (0);
 }
