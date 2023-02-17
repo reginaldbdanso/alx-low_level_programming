13475c37a35bf0fc404d474cb5ab1448fb48f369
reginaldbdanso <reginaldbdanso@gmail.com>
2023-02-17 09:03:50 -0800

    Added script 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num;
+num = 48;
+
+while (num < 58)
+{
+putchar(num);
+num++;
+
+}
+putchar('\n');
+return (0);
+}
