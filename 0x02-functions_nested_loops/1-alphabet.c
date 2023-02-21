b5d55d55c610cfd7e8660838ff02d92880365557
reginaldbdanso <reginaldbdanso@gmail.com>
2023-02-21 12:03:36 -0800

    Added script 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*void print_alphabet(void);*/
+
+/**
+ *Function to print out alphabets in lowercase
+ */
+
+void print_alphabet(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+
+return;
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,3 @@
+int _putchar (char c);
+void print_alphabet(void);
+
