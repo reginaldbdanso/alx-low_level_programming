b6025f46bc194d32c55009aeb0bc4d4b97d2da14
reginaldbdanso <reginaldbdanso@gmail.com>
2023-02-21 08:52:46 -0800

    Added script 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+
+return (0);
+
+}
+
