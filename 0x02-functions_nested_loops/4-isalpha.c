864b0a21084f3c32d6b6d961df3bba10bbe1d1a4
reginaldbdanso <reginaldbdanso@gmail.com>
2023-02-21 13:37:56 -0800

    Added script 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
- * _islower - checks for lowercase character
+ * _isalpha - checks for alphabetic character
  * @c: The character to print
  *Return: 1 (success)
  */
 
-int _islower(int c)
+int _isalpha(int c)
 {
 
-int ret;
-
-if (c >= 'a' && c <= 'z')
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 {
 return (1);
 }
