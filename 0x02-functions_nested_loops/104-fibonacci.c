4fa22e8645e028cd8c543a5466c11d4b95667c37
reginaldbdanso <reginaldbdanso@gmail.com>
2023-02-22 11:52:49 -0800

    Added script 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,29 +9,40 @@
  */
 int main(void)
 {
-long int a;
-long int b;
-int i;
-long int c;
+unsigned long int i;
+unsigned long int a;
+unsigned long int b;
+unsigned long int a1;
+unsigned long int a2;
+unsigned long int b1;
+unsigned long int b2;
 a = 1;
 b = 2;
 
-printf("%lu, %lu, ", a, b);
-
-for (i = 3; i <= 98; i++)
+printf("%lu", a);
+i = 1;
+while (i < 91)
 {
-c = a + b;
-printf("%lu", c);
-
-if (i != 98)
+printf(", %lu", b);
+b += a;
+a = b - a;
+i++;
+}
+a1 = a / 1000000000;
+a2 = a % 1000000000;
+b1 = b / 1000000000;
+b2 = b % 1000000000;
+i = 92;
+while (i < 99)
 {
-printf(", ");
+printf(", %lu", b1 + (b2 / 1000000000));
+printf("%lu", b2 % 1000000000);
+b1 = b1 + a1;
+a1 = b1 - a1;
+b2 = b2 + a2;
+a2 = b2 - a2;
+i++;
 }
-a = b;
-b = c;
-}
-
 printf("\n");
-
 return (0);
 }
