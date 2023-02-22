ba842419422281aec5eb6f18dc28ba3444bbcb68
reginaldbdanso <reginaldbdanso@gmail.com>
2023-02-21 18:16:16 -0800

    Added script 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result.
+ *
+ *@m: first interger
+ *@n: second integer
+ *
+ * Return: result
+ */
+
+int add(int m, int n)
+{
+	int result;
+
+	result = m + n;
+
+	return (result);
+}
