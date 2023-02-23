d628ee5bccb4ddb8ed4bd81a8b8983457a9739e0
reginaldbdanso <reginaldbdanso@gmail.com>
2023-02-22 16:38:05 -0800

    Fixed script 2-largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,20 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+int largest;
 
-    if ((a > b && b > c) || (a > c && c > b))
-    {
-        largest = a;
-    }
-    else if ((b > a && a > c) || (b > c && c > a))
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
+if ((a > b && b > c) || (a > c && c > b))
+{
+largest = a;
+}
+else if ((b > a && a > c) || (b > c && c > a))
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
 
-    return (largest);
+return (largest);
 }
