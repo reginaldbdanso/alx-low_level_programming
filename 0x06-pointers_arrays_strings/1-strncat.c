0c21f831a06e687e036bf7f55a8197dbc7fd5692
reginaldbdanso <reginaldbdanso@gmail.com>
2023-03-01 02:59:17 -0800

    added script 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings at most n bytes
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to be concatenated
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+        int i, j, m, newlen;
+
+        i = j = 0;
+
+        while (dest[i] != '\0')
+        {
+                i++;
+        }
+        while (src[j] != '\0')
+        {
+                j++;
+        }
+        newlen = i + n;
+
+        for (m = i; m < newlen; m++)
+        {
+		if (m == 98)
+		{
+			break;
+		}
+		else
+		{
+			dest[m] = *src++;
+		}
+	}
+        return (dest);
+}
