93a05f05038970ae1f8400fa655313368c8202c0
reginaldbdanso <reginaldbdanso@gmail.com>
2023-03-06 20:19:07 +0300

    Fixed script 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,28 +1,21 @@
 #include "main.h"
-
 /**
- *_memcpy - copies memory area
- *@dest: destination pointer
- *@src: source pointer
+ *_memcpy - a function that copies memory area
+ *@dest: memory where is stored
+ *@src: memory where is copied
  *@n: number of bytes
  *
- *Return: dest
+ *Return: copied memory with n byted changed
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int a;
+	int r = 0;
+	int i = n;
 
-	a = 0;
-	while (a < n)
+	for (; r < i; r++)
 	{
-		if (a == 5)
-		{
-			dest[a] = src[a + 1];
-			a++;
-			continue;
-		}
-		dest[a] = src[a];
-		a++;
+		dest[r] = src[r];
+		n--;
 	}
 	return (dest);
 }
