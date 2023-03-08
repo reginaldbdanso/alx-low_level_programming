6298a8ad6bbaae83fa8368a6e7ec6e1ae44bfeea
reginaldbdanso <reginaldbdanso@gmail.com>
2023-03-09 00:21:49 +0300

    Added script 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,20 +6,38 @@
  * @n: number
  * Return: integer
  */
+int _sqrt_recursion_helper(int n, int start, int end);
+
 int _sqrt_recursion(int n)
 {
-	int root;
-
 	if (n < 0)
 		return (-1);
 	if (n == 0 || n == 1)
 		return (n);
 
-	root = _sqrt_recursion(n / 2);
-	if (n == root * root)
-		return (root);
-	else if (n < root * root)
-		return (_sqrt_recursion(n / 2));
+	return (_sqrt_recursion_helper(n, 0, n / 2 + 1));
+}
+
+/**
+ * _sqrt_recursion_helper - helps by restricting the range\
+ for the recursive search
+ * @n: number
+ * Return: integer
+ */
+int _sqrt_recursion_helper(int n, int start, int end)
+{
+	int mid, root;
+
+	if (start > end)
+		return (-1);
+
+	mid = (start + end) / 2;
+	root = mid * mid;
+
+	if (root == n)
+		return (mid);
+	else if (root < n)
+		return (_sqrt_recursion_helper(n, mid + 1, end));
 	else
-		return (_sqrt_recursion(n / 2 + 1));
+		return (_sqrt_recursion_helper(n, start, mid - 1));
 }
