0f7cd68a61d34d1a6694655a7e5196c8bf085841
reginaldbdanso <reginaldbdanso@gmail.com>
2023-03-09 02:17:11 +0300

    Added script 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,43 +1,31 @@
 #include "main.h"
 
 /**
+ * _sqrt_recursion_helper - helps by restricting the range\
+ for the recursive search
+ * @n: number
+ * @root: number
+ * Return: integer
+ */
+int _sqrt_recursion_helper(int n, int root)
+{
+	if (root * root > n)
+		return (-1);
+	if (root * root == n)
+		return (root);
+	return (_sqrt_recursion_helper(n, root + 1));
+}
+
+/**
  * _sqrt_recursion - returns the natural square root\
   of a number.
  * @n: number
  * Return: integer
  */
-int _sqrt_recursion_helper(int n, int start, int end);
 
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	if (n == 0 || n == 1)
-		return (n);
-
-	return (_sqrt_recursion_helper(n, 0, n / 2 + 1));
-}
-
-/**
- * _sqrt_recursion_helper - helps by restricting the range\
- for the recursive search
- * @n: number
- * Return: integer
- */
-int _sqrt_recursion_helper(int n, int start, int end)
-{
-	int mid, root;
-
-	if (start > end)
-		return (-1);
-
-	mid = (start + end) / 2;
-	root = mid * mid;
-
-	if (root == n)
-		return (mid);
-	else if (root < n)
-		return (_sqrt_recursion_helper(n, mid + 1, end));
-	else
-		return (_sqrt_recursion_helper(n, start, mid - 1));
+	return (_sqrt_recursion_helper(n, 0));
 }
