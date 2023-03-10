31cc764049778ac1c72e38556a900797ba0ea2ef
reginaldbdanso <reginaldbdanso@gmail.com>
2023-03-11 01:06:01 +0300

    Added script 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+
+/**
+ * main - Entry point
+ * Description: Multiplies two numbers.
+ * @argc: - argument count
+ * @argv: - array of strings
+ *Return: 0 Always (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int counter;
+
+	counter = 0;
+	if (argc == 1)
+		printf("Error\n");
+	else
+		printf("%d\n", atoi(argv[counter + 1]) * atoi(argv[counter + 2]));
+
+	return (0);
+}
