58283f4b528fb14471a8a9391903bca7393c0575
reginaldbdanso <reginaldbdanso@gmail.com>
2023-03-11 01:25:09 +0300

    Added script 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,8 +1,50 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int i, d, n, lengthh, f, number;
 
+	i = 0;
+	d = 0;
+	n = 0;
+	lengthh = 0;
+	f = 0;
+	number = 0;
+
+	while (s[lengthh] != '\0')
+		lengthh++;
+
+	while (i < lengthh && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			number = s[i] - '0';
+			if (d % 2)
+				number = -number;
+			n = n * 10 + number;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+	else
+		return (n);
+}
 /**
  * main - Entry point
  * Description: Multiplies two numbers.
@@ -18,7 +60,7 @@ int main(int argc, char *argv[])
 	if (argc == 1 || argc > 3)
 		printf("Error\n");
 	else
-		printf("%d\n", atoi(argv[counter + 1]) * atoi(argv[counter + 2]));
+		printf("%d\n", _atoi(argv[counter + 1]) * _atoi(argv[counter + 2]));
 
 	return (0);
 }
