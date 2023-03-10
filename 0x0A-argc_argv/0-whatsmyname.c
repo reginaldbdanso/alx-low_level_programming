47a78ba831b26e2dc018a5a558cc420dc5ab2747
reginaldbdanso <reginaldbdanso@gmail.com>
2023-03-11 00:02:08 +0300

    My first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: - argument count
+ * @argv: - array of strings
+ *Return: 0 Always (Success)
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
