0bbe0173b824ac08b4707cccc467a3705d7191f7
reginaldbdanso <reginaldbdanso@gmail.com>
2023-03-11 18:45:46 +0300

    Fixed script 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -68,7 +68,7 @@ int main(int argc, char *argv[])
 				if (!((*(argv[i] + j) >= '0') && (*(argv[i] + j) <= '9')))
 				{
 					printf("Error\n");
-					return (0);
+					return (1);
 				}
 
 				else
