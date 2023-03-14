e5ef5417934b755705389ba33163c1b4b256df33
reginaldbdanso <reginaldbdanso@gmail.com>
2023-03-14 08:10:36 -0700

    added file 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in\
+ memory, which contains a copy of the string given as a parameter.
+ * @str: - string
+ *Return: pointer to new space in memory
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (str == NULL)
+		return (NULL);
+	ptr = (char *)malloc(strlen(str) * sizeof(char));
+	if (ptr == NULL)
+		printf("Memory allocation failed\n");
+	else
+	{
+		for (i = 0; i < strlen(str); i++)
+		ptr[i] = str[i];
+	}
+	return (ptr);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -22,5 +22,6 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 
 #endif
