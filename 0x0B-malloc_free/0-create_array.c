de9242baae1ad22409e680276e01c865d62b256e
reginaldbdanso <reginaldbdanso@gmail.com>
2023-03-14 06:33:55 -0700

    fixed file 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,16 +14,8 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i;
 
-	if (size == 0)
-	{
-		ptr == NULL;
-		return (ptr);
-	}
-	else
-	{
-		ptr = malloc(size * sizeof(int));
-		for (i = 0; i < size; i++)
-			ptr[i] = c;
-		return (ptr);
-	}
+	ptr = malloc(size * sizeof(int));
+	for (i = 0; i < size; i++)
+		ptr[i] = c;
+	return (ptr);
 }
