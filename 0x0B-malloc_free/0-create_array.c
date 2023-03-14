51196490be4c8a6ac28492d344f2dfc56eecdd0e
reginaldbdanso <reginaldbdanso@gmail.com>
2023-03-14 06:13:04 -0700

    My first commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars, and initializes\
+ it with a specific char.
+ * @size: - amount of memory to allocate
+ * @c: character to use
+ *Return: 0 Always (Success)
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i;
+
+	ptr = malloc(size * sizeof(int));
+	for (i = 0; i < size; i++)
+		ptr[i] = c;
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+#This Readme file will describe what this Repo is about.
+0x09-static_libraries
