35b7ab0be4c7963dd83bc4d1b1f6be4ba713331d
reginaldbdanso <reginaldbdanso@gmail.com>
2023-03-14 14:45:53 -0700

    Added script 3

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -24,5 +24,6 @@ char *_strstr(char *haystack, char *needle);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
 
 #endif
