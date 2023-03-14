c63c88ac535eede065b21693949e56fec736d668
reginaldbdanso <reginaldbdanso@gmail.com>
2023-03-14 07:18:48 -0700

    fixed file 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,12 +14,9 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i;
 
-	/*if (size == 0)*/
-	/*	return (NULL);*/
-
-	ptr = malloc(size * sizeof(char));
-	if (size == 0)
-		ptr = NULL;
+	ptr = (char*)malloc(size * sizeof(char));
+	if (ptr == NULL)
+		printf("Memory allocation failed\n");
 	else
 	{
 		for (i = 0; i < size; i++)
