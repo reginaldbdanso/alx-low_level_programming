b1b0f342377c7bfe1fe5b151afbf2e492a9207d7
reginaldbdanso <reginaldbdanso@gmail.com>
2023-03-15 16:32:04 -0700

    Added script 100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,80 +1,46 @@
-
+#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
- */
-int _atoi(char *s)
-{
-	int i, d, n, lengthh, f, number;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	lengthh = 0;
-	f = 0;
-	number = 0;
-
-	while (s[lengthh] != '\0')
-		lengthh++;
-
-	while (i < lengthh && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			number = s[i] - '0';
-			if (d % 2)
-				number = -number;
-			n = n * 10 + number;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
-
-	if (f == 0)
-		return (0);
-	else
-		return (n);
-}
-/**
  * main - Entry point
- * Description: Adds positive numbers.
+ * Description: prints the minimum number of coins to make\
+ change for an amount of money.
  * @argc: - argument count
  * @argv: - array of strings
  *Return: 0 Always (Success)
  */
 int main(int argc, char *argv[])
 {
-	int i, result;
-
-	result = 0;
-	if (argc != 1)
+	int cents;
+	int coins;
+	
+	if (argc != 2)
 	{
-		for (i = 1; i < argc; i++)
-		{
-			if (!((*argv[i] >= '0') && (*argv[i] <= '9')))
-			{
-				printf("Error\n");
-				return (0);
-			}
-
-			else
-				result += _atoi(argv[i]);
-		}
-		printf("%d\n", result);
+		printf("Error\n");
+        	return 1;
 	}
-	else
-		printf("0\n");
 
-	return (0);
+	cents = atoi(argv[1]);
+	if (cents < 0)
+	{
+		printf("0\n");
+		return 0;
+	}
+
+	coins = 0;
+	coins += cents / 25;
+	cents %= 25;
+	coins += cents / 10;
+	cents %= 10;
+	coins += cents / 5;
+	cents %= 5;
+	coins += cents / 2;
+	cents %= 2;
+	coins += cents;
+
+	printf("%d\n", coins);
+
+	return 0;
 }
