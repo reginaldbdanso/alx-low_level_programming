e55373bdce1c08a03641764297711a10686b0945
reginaldbdanso <reginaldbdanso@gmail.com>
2023-03-16 14:24:00 -0700

    Added script 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,7 +3,7 @@
 
 /**
  * _calloc - allocates memory for an array using malloc
- * @nmemb: number of elements 
+ * @nmemb: number of elements
  * @size: array size
  * Return: pointer to new allocated space in memory
  */
@@ -22,7 +22,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	for (i = 0; i < (int)nmemb; i++)
 	{
-	  ptr[i] = 0;
+		ptr[i] = 0;
 	}
 
 	return (ptr);
