ed34ced12d0acd0ca472de2b04ccaecde8db4fa5
reginaldbdanso <reginaldbdanso@gmail.com>
2023-03-16 12:47:19 -0700

    Added script 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: size of memory
+ * Return: pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
