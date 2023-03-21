6839290243f01c22c777d197e0914c77eb8b0816
reginaldbdanso <reginaldbdanso@gmail.com>
2023-03-21 07:00:49 -0700

    Added header file for task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,5 @@
-#include "main.h"
-#include <unistd.h>
+#include <stdio.h>
+
 
 /**
  * main - prints the name of the file it was compiled from, followed by a new line.
@@ -8,35 +8,6 @@
  */
 int main(void)
 {
-	_puts(__BASE_FILE__);
+	printf("%s\n", __BASE_FILE__);
 	return (0);
-}
-
-/**
- * _puts - prints a string, followed by a new line, to stdout.
- * @str: pointer
- * Return:
- */
-void _puts(char *str)
-{
-
-	while (*str != 0)
-	{
-		_putchar(*str);
-		str++;
-	}
-	_putchar('\n');
-	return;
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
 }
