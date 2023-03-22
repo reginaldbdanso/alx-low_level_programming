60d740790ec57b65cfc533d9698959438ff98e02
reginaldbdanso <reginaldbdanso@gmail.com>
2023-03-21 18:19:26 -0700

    Added .c file for task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints a struct dog
+ * @d: first parameter
+ * Return:
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		exit (98);
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	  printf("Name: %s\n", d->name);
+	  printf("Age: %f\n", d->age);
+	  printf("Owner: %s\n", d->owner);
+
+}
