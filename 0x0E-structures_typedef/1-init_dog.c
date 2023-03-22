64d12f94a2907abe07a6b09f8670433bc7a741e9
reginaldbdanso <reginaldbdanso@gmail.com>
2023-03-21 17:42:31 -0700

    Added .c file for task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,9 +12,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-  /*	d = malloc(sizeof(struct dog));*/
-	if (d == NULL)
-		exit (98);
+	struct dog *ptr;
+	ptr = d;
+	ptr = malloc(sizeof(struct dog));
+        if (ptr == NULL)
+                exit (98);
 	else
 	{
 		d->name = name;
