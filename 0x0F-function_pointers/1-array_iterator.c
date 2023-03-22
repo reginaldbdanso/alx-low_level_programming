8b371f333491e4b59a4bd71f6296c46c5a3e9327
reginaldbdanso <reginaldbdanso@gmail.com>
2023-03-22 17:37:37 +0000

    Updated .c file for task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,7 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if ((array == NULL) || (size == 0)|| (action == NULL))
+	if ((array == NULL) || (action == NULL))
 		return;
 	for (i = 0; i < size; i++)
 	{
