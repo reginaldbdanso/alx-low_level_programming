95c6e974c51ddfdfbc56a166de30929fcce8d61f
reginaldbdanso <reginaldbdanso@gmail.com>
2023-03-23 15:06:55 +0000

    Added .c file for advanced task

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: number of arguments
+ * @argv: array of strings
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+int i, bytes;
+
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+
+bytes = atoi(argv[1]);
+
+if (bytes < 0)
+{
+printf("Error\n");
+exit(2);
+}
+
+char *main_code = (char *) main;
+
+for (i = 0; i < bytes; i++)
+{
+printf("%.2hhx", main_code[i]);
+if (i == bytes - 1)
+printf("\n");
+else
+printf(" ");
+}
+
+return (0);
+}
