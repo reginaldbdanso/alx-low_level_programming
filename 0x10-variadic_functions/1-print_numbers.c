446d77f28f6d4f86f2c8d52e84bffd4dc49afb2d
reginaldbdanso <reginaldbdanso@gmail.com>
2023-03-23 19:27:54 +0000

    Added .c file for task 1 and updated .h file

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+
+/**
+ * print_numbers - prints numbers, followed by a new line
+ *
+ * @separator: string
+ * @n: argument count
+ * Return: 0 Always (Success)
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list list_args;
+
+	if (n == 0)
+		return;
+
+	va_start(list_args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(list_args, int));
+		if (i != n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(list_args);
+}
