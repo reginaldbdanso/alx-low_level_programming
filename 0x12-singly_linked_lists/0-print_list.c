fde3e1d49e95fa5332ccae909ff9eac2b8f549fe
reginaldbdanso <reginaldbdanso@gmail.com>
2023-03-30 19:04:47 +0000

    Updated 0-print_list and header file

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,11 +15,9 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-		{
 			printf("[%u] %s\n", h->len, h->str);
-			h = h->next;
-			counter++;
-		}
+		h = h->next;
+		counter++;
 	}
 	return (counter);
 }
