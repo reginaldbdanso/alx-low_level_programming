7d83b585adeefefc69c7f60fbfce697d338c6c94
reginaldbdanso <reginaldbdanso@gmail.com>
2023-04-03 22:03:44 +0000

    Fixed 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp1, *temp2 = NULL, *new;
 	unsigned int i = 0;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return (NULL);
 
 	temp1 = temp2 = *head;
