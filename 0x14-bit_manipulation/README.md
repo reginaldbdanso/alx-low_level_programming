4c76c74df9483ac04b6753416b14e67831162653
reginaldbdanso <reginaldbdanso@gmail.com>
2023-04-06 05:19:54 -0700

    My first commit

---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,2 @@
+#This Readme file will describe what this Repo is about.
+bit manipulation
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+
+#endif
