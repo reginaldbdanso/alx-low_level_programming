50426a1a348fd2977abdd70b7cfbe921bbf2984d
reginaldbdanso <reginaldbdanso@gmail.com>
2023-04-10 10:49:47 +0000

    Updated 1-print_binary.c file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,5 +12,8 @@ void print_binary(unsigned long int n)
 	if (n > 1)
 		print_binary(n >> 1);
 	/*Print the least significant bit of num in binary form */
-	_putchar(n % 2 + '0');
+	if (n & 1)
+		_putchar('1');
+	else
+		_putchar('0');
 }
