7bb8f98e8377d42facece470f699c51bf732feb5
reginaldbdanso <reginaldbdanso@gmail.com>
2023-04-10 10:36:45 +0000

    Updated 1-print_binary.c file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,7 +10,7 @@ void print_binary(unsigned long int n)
 {
 	/*If num is greater than 1, continue the recursion*/
 	if (n > 1)
-		print_binary(n / 2);
+		print_binary(n >> 1);
 	/*Print the least significant bit of num in binary form */
 	_putchar(n % 2 + '0');
 }
