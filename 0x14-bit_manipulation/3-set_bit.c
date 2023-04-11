904f8fd049b3aef530aaa15d9f7e047014e36c86
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-04-11 17:41:39 +0000

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,6 +10,7 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int sized = (sizeof(unsigned long int) * 8);
+	unsigned long int wan = 1;
 
 	/*Check if the index is out of bounds*/
 	if (index >= sized)
@@ -17,7 +18,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 
 	/*Right shift the bits index times*/
 	/*Perform a bitwise AND with 1 to get the bit at index*/
-	*n = ((*n >> index) | 1024);
+	*n = (*n | (wan >> index));
 
 	return (1);
 }
