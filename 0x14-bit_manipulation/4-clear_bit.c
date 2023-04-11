ed03649fceceabb3cb39c8d4c01dd9034999c1b6
reginaldbdanso <reginaldbdanso@gmail.com>
2023-04-11 20:34:31 +0000

    Updated 4-clear_bit.c file

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * clear_bit - returns the value of a bit at a given index.
+ * clear_bit - sets the value of a bit to 0 at a given index.
  * @n: nunber to be converted
  * @index: position of bit
- * Return: the value of the bit at index
- * index or -1 if an error occured
+ * Return: 1 if it worked, or -1 if an error occurred
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
