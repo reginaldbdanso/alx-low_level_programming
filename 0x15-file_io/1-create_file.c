7a750c0cbe7bc1d9cd561e3626307809df966569
reginaldbdanso <reginaldbdanso@gmail.com>
2023-04-13 22:55:21 +0000

    Added file 1 and 2, and updated main.h

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file.
+ * @filename: name of file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	FILE *fp;
+
+	if (filename == NULL)
+		return (-1);
+
+	fp = fopen(filename, "w");
+	if (fp == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+		fputs(text_content, fp);
+
+	fclose(fp);
+	return (1);
+}
