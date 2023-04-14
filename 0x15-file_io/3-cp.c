098a5715762eacbe65f0aebb84d8ec71b4f06d0f
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-04-14 14:09:15 +0000

    Update 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,32 +6,33 @@
  * file to another file
  * @argc: number of arguments
  * @argv: arrayof strings
- * Return: 1 on success, -1 on failure
+ * Return: 0 (Success)
  */
 
 #define BUF_SIZE 1024
 
 int main(int argc, char *argv[]) 
 {
+	int fd_from, fd_to;
+	char buf[BUF_SIZE];
+    ssize_t bytes_read, bytes_written;
+
     if (argc != 3) {
         dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
         exit(97);
     }
 
-    int fd_from = open(argv[1], O_RDONLY);
+    fd_from = open(argv[1], O_RDONLY);
     if (fd_from == -1) {
         dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
         exit(98);
     }
 
-    int fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if (fd_to == -1) {
         dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
         exit(99);
     }
-
-    char buf[BUF_SIZE];
-    ssize_t bytes_read, bytes_written;
 
     while ((bytes_read = read(fd_from, buf, BUF_SIZE)) > 0) {
         bytes_written = write(fd_to, buf, bytes_read);
@@ -60,5 +61,5 @@ int main(int argc, char *argv[])
         exit(100);
     }
 
-    return 0;
+    return (0);
 }
