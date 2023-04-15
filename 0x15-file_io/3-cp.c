b3ceda7ecf03018263f4fb0c4018965999f6c153
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-04-15 13:09:51 +0000

    Updated 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,35 +15,36 @@ int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
 	char buf[BUF_SIZE];
-    ssize_t bytes_read, bytes_written;
+	ssize_t bytes_read, bytes_written;
 
-    if (argc != 3) {
-        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-        exit(97);
-    }
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 
-    fd_from = open(argv[1], O_RDONLY);
-    if (fd_from == -1) {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
-    }
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 
     fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (fd_to == -1) {
+    if (fd_to == -1)
+    {
         dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
         exit(99);
     }
 
-    while ((bytes_read = read(fd_from, buf, BUF_SIZE)) > 0) {
+    while ((bytes_read = read(fd_from, buf, BUF_SIZE)) > 0)
+    {
         bytes_written = write(fd_to, buf, bytes_read);
-        if (bytes_written == -1) {
+        if ((bytes_written == -1) || (bytes_written != bytes_read)) {
             dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
             exit(99);
         }
-        if (bytes_written != bytes_read) {
-            dprintf(STDERR_FILENO, "Error: Incomplete write to %s\n", argv[2]);
-            exit(99);
-        }
+        
     }
 
     if (bytes_read == -1) {
