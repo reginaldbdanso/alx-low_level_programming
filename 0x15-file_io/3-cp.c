7fbe7412148c4cf60a1fca6751f93e834283a8e7
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-04-16 01:03:55 +0000

    Updated 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
+ * closing_time - closes the file descriptors
+ * @fd_from: input file descriptor
+ * @fd_to: output file descriptor
+ * Return: 
+ */
+
+void closing_time(int fd_from, int fd_to)
+{
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+}
+/**
  * main - entry point
  * Description: a program that copies the content of a
  * file to another file
@@ -9,12 +29,10 @@
  * Return: 0 (Success)
  */
 
-#define BUF_SIZE 1024
-
 int main(int argc, char *argv[]) 
 {
 	int fd_from, fd_to;
-	char buf[BUF_SIZE];
+	char buf[1024];
 	ssize_t bytes_read, bytes_written;
 
 	if (argc != 3)
@@ -22,22 +40,19 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-
-	while ((bytes_read = read(fd_from, buf, BUF_SIZE)) > 0)
+	while ((bytes_read = read(fd_from, buf, 1024)) > 0)
 	{
 		bytes_written = write(fd_to, buf, bytes_read);
 		if ((bytes_written == -1) || (bytes_written != bytes_read))
@@ -46,24 +61,11 @@ int main(int argc, char *argv[])
         		exit(99);
 		}
         }
-
 	if (bytes_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
-	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-
-	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
-
+	closing_time(fd_from, fd_to);
 	return (0);
 }
