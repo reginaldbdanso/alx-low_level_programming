4aeeee4e0fe336e6eec11e1bf1ff78cd30b8b457
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-04-16 11:06:10 +0000

    Updated 2-appen_text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 		fprint(fp, %s, text_content);
 	else
-	return (1);
+		return (1);
 
 	fclose(fp);
 	return (1);
