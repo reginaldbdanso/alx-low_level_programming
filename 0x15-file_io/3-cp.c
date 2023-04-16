c93baca454ab8b5cfeaa1e5b2a6fdddc2586448a
reginaldbdanso <reginaldbdanso@gmail.com>
2023-04-16 01:24:32 +0000

    Updated 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -46,7 +46,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 00644);
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
