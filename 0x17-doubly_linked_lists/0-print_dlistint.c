17bd3f2cf481da85e893140e533fe38d52feb8d3
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-06-15 15:14:24 +0000

    Added 0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list.
+ * @h: head node receiving argument
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t counter = 0;
+
+	while (h != NULL)
+	{
+		/*if (h->next != NULL)*/
+		printf("%d\n", h->n);
+		/*else*/
+			/*printf("0\n");*/
+
+		h = h->next;
+		counter++;
+	}
+	return (counter);
+}
