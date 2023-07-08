0f4ffa1f00ccb77d04c10deb3b2d64644c52e565
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-07-08 05:35:06 +0000

    Added 6-hash_table_delete.c

---
+++ 0x1A-hash_tables /6-hash_table_delete.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - clears a hash table
+ * @ht: pointer to the hash table
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *point1;
+	hash_node_t *poiint2;
+	unsigned long int i;
+
+	if (ht != NULL)
+
+	for (i = 0; i < ht->size; i++)
+	{
+		point1 = ht->array[i];
+		while (point1 != NULL)
+		{
+			point2 = point1->next;
+			free(point1->key);
+			free(point1->value);
+			free(point1);
+			point1 = point2;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
